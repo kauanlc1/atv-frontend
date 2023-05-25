3e6bc66cf00cbdaa023737b390730b9296ba4295
kauanlc1 <116395890+kauanlc1@users.noreply.github.com>
2023-05-25 20:00:38 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [itens, setItens] = useState([]);
+  const [novoItem, setNovoItem] = useState('');
+
+  const adicionarItem = () => {
+    if (novoItem.trim() !== '') {
+      setItens([...itens, novoItem]);
+      setNovoItem('');
+    }
+  };
+
+  const removerItem = (index) => {
+    const novosItens = [...itens];
+    novosItens.splice(index, 1);
+    setItens(novosItens);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Lista de Compras</h2>
+      <input
+        type="text"
+        value={novoItem}
+        onChange={(e) => setNovoItem(e.target.value)}
+        placeholder="Digite um item"
+      />
+      <button onClick={adicionarItem}>Adicionar</button>
+      <ul>
+        {itens.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button onClick={() => removerItem(index)}>Remover</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
